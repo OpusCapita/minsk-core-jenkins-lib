ae711fdb9791d689cb3107fe8de27a6d4ef52f65
Alexey Sergeev <sab@scand.com>
2017-12-20 09:46:28 +0300

    Fixing bugs

--- vars/slack.groovy
+++ vars/slack.groovy
@@ -18,7 +18,7 @@ def sendNotification() {
                 message = "${env.BUILD_USER_ID}'s build (<${env.BUILD_URL}|${env.BUILD_DISPLAY_NAME}>; push) in <${ref}|${ref - "https://github.com/"}> (${env.BRANCH_NAME})"
             } else {
                 def scmInfo = checkout scm
-                message = "${env.BUILD_USER_ID}'s build (<${env.BUILD_URL}|${env.BUILD_DISPLAY_NAME}>) no changes in repo <${scmInfo.GIT_URL}|${scmInfo.GIT_URL - "https://github.com/"} (${env.BRANCH_NAME})"
+                message = "${env.BUILD_USER_ID}'s build (<${env.BUILD_URL}|${env.BUILD_DISPLAY_NAME}>) no changes in repo <${scmInfo.GIT_URL}|${scmInfo.GIT_URL - "https://github.com/"}> (${env.BRANCH_NAME})"
             }
         }
         message = "${(result == 'FAILURE')?'Failed':'Success'}: ${message}"
