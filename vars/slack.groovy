669d3112d9e617621e14bdf9af64a6f219d050ea
Alexey Sergeev <sab@scand.com>
2017-12-21 08:19:27 +0300

    Experimenting with Jenkins build process

--- vars/slack.groovy
+++ vars/slack.groovy
@@ -12,10 +12,10 @@ def sendNotification() {
         def scmInfo = checkout scm
         // extracting branch and whole repo build server job urls if possible
         def regex = "((.+/job/.+/)job/.+/).+/"
-        def findings = (url =~ /$regex/)
+        def findings = (env.BUILD_URL =~ /$regex/)
         if (findings.matches()) {
-            branchBuildUrl = findings.group(1)
-            repositoryBuildUrl = findings.group(2)
+            def branchBuildUrl = findings.group(1)
+            def repositoryBuildUrl = findings.group(2)
             message = "${env.BUILD_USER_ID}'s build (<${env.BUILD_URL}|${env.BUILD_DISPLAY_NAME}>) in <${repositoryBuildUrl}|${gitHubUtils.extractRepositoryOwnerAndName(scmInfo.GIT_URL)}> (<${branchBuildUrl}}|${env.BRANCH_NAME}>)"
         } else {
             message = "${env.BUILD_USER_ID}'s build (<${env.BUILD_URL}|${env.BUILD_DISPLAY_NAME}>) in ${gitHubUtils.extractRepositoryOwnerAndName(scmInfo.GIT_URL)} (${env.BRANCH_NAME})"
